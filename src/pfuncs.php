cb6aa17ce13ab20d8acd4ea061d0a92fa9d44dec
chase <chase@life360.com>
2019-01-05 11:12:31 -0800

    ok

--- src/pfuncs.php
+++ src/pfuncs.php
@@ -1,6 +1,8 @@
 <?php
 foreach (scandir(__DIR__) as $_) {
-    include_once(realpath(__DIR__ . "/$_"));
+    if (strpos($_, '.') !== 0) {
+        include_once(realpath(__DIR__ . "/$_"));
+    }
 }
 
 unset($_);
